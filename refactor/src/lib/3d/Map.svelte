a0878589c46aebd802742b1d8558daf90206c0a3
Veikkan <pierre.semanne@gmail.com>
2024-08-30 23:08:20 +0200

    feat: refactor map

--- refactor/src/lib/3d/Map.svelte
+++ refactor/src/lib/3d/Map.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+    import { T } from "@threlte/core";
     import Ground from "./map/Ground.svelte";
     import Light from "./map/Light.svelte";
     import Walls from "./map/Walls.svelte";
 </script>
 
-
-<Ground />
-<Light />
-<Walls />
+<T.Group>
+    <Ground />
+    <Light />
+    <Walls />
+</T.Group> 
---
+++ refactor/src/lib/3d/map/Light.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { T } from '@threlte/core';
+</script>
+
+<T.Group>
+    <T.AmbientLight intensity={0.5} />
+    <T.DirectionalLight position={[10, 10, 10]} intensity={1} castShadow />
+</T.Group>
