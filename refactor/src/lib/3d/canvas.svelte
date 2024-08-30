0ec72d4e575b546302615df044a83033642367b8
Veikkan <pierre.semanne@gmail.com>
2024-08-30 22:23:20 +0200

    basic svelte pages

---
+++ refactor/src/lib/3d/canvas.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Canvas } from '@threlte/core';
+    import Ground from './map/ground.svelte';
+  
+</script>
+CANVAS
+<Canvas>
+    <Ground />
+</Canvas>
---
+++ refactor/src/routes/game/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Ui from '$lib/ui/Ui.svelte';
+    import Canvas from '$lib/3d/canvas.svelte';
+</script>
+
+
+<Ui />
+<Canvas />
