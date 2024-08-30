581e9089fa91548bb53d977fa4e216104bfa77fa
Veikkan <pierre.semanne@gmail.com>
2024-08-30 21:20:17 +0200

    feat: add the font block

--- refactor/tailwind.config.js
+++ refactor/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Block: ['Block', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
 
