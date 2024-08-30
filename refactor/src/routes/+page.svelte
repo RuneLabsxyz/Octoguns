53b47d066056604132212d15156b3269ab5871c1
Veikkan <pierre.semanne@gmail.com>
2024-08-30 21:26:56 +0200

    feat: simple home page + rounting

--- refactor/src/routes/+page.svelte
+++ refactor/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1 class="font-Block">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+
+</script>
+
+
+<div class="flex flex-col justify-center items-center h-screen">
+    <div class="text-9xl">
+        <h1 class="font-Block">OCTO</h1>
+        <h2 class="font-Block">GUNS</h2>
+    </div>
+    <button class="p-5 pt-20" on:click={() => goto('/games')}>
+        Play
+    </button>
+</div>
+
