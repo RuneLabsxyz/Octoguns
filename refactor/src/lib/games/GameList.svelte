6ead25610129a9707257cfa7863bea26f032ab62
Veikkan <pierre.semanne@gmail.com>
2024-09-03 06:31:15 +0200

    fix: make sure setup is always ran

---
+++ refactor/src/lib/games/GameList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let global;
+
+    $: pendingSessions = $global.pending_sessions;
+
+    async function joinSession(session: any) {
+        console.log(`Joining session: ${session.value}`);
+    }
+</script>
+
+<div class="flex flex-col items-center h-screen text-center">
+    <h1 class="font-Block text-6xl py-10">Octo Guns</h1>
+
+    {#if pendingSessions}
+        <div class="w-full max-w-3xl border-2 border-black p-4 overflow-y-auto">
+            {#each pendingSessions as session}
+                <div class="flex justify-between items-center py-2">
+                    <p class="flex-grow text-left">{session.value}</p>
+                    <button 
+                        class="ml-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition"
+                        on:click={() => joinSession(session)}>
+                        Join
+                    </button>
+                </div>
+            {/each}
+        </div>
+    {/if}
+</div>
--- refactor/src/routes/games/+page.svelte
+++ refactor/src/routes/games/+page.svelte
@@ -1,8 +1,53 @@
 <script lang="ts">
+    import { derived } from "svelte/store";
+    import { setupStore } from "../../stores/dojoStore";
+    import { createComponentValueStore } from "../../dojo/componentValueStore";
+    import GameList from "$lib/games/GameList.svelte";
 
+    $: ({ clientComponents, torii, burnerManager, client } = $setupStore);
+
+    $: account = burnerManager.getActiveAccount();
+
+	$: globalentity = derived(setupStore, ($store) =>
+		$store
+		? torii.poseidonHash([BigInt(0).toString()])
+		: undefined
+	);
+
+	$: playerEntity = derived(setupStore, ($store) =>
+		$store
+		? torii.poseidonHash([account?.address])
+		: undefined
+	);
+
+	$: global = createComponentValueStore(clientComponents.Global, globalentity);
+	$: player = createComponentValueStore(clientComponents.Player, playerEntity);
+    
 </script>
 
 
 <div>
     GAMES
 </div>
+
+
+{#if $global}
+    <div class="flex justify-center items-center">
+        <GameList {global} />
+    </div>
+
+    <button
+        on:click={async () => {
+        const account = burnerManager.getActiveAccount();
+        if (account) {
+            await client.start.create({ account });
+        } else {
+            console.error("No active account found");
+        }
+    }}>Create Game</button>
+
+{/if}
+
+
+
+
