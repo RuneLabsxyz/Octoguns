df163377c9a1aa5742bcc60ff40c3b9a5f3435be
Veikkan <pierre.semanne@gmail.com>
2024-09-16 17:48:38 +0200

    feat(3d): add better floor

--- refactor/src/lib/3d/components/map/Ground.svelte
+++ refactor/src/lib/3d/components/map/Ground.svelte
@@ -1,16 +1,39 @@
 <script lang="ts">
   import { T } from '@threlte/core'
-  import * as THREE from 'three'
+  import { FakeGlowMaterial } from '@threlte/extras'
+  import { Edges } from '@threlte/extras'
+  import { Color } from 'three'
 
-  const textureLoader = new THREE.TextureLoader()
-  const tileTexture = textureLoader.load('/sprites/ground.png')
+  const monochromeColors = [
+    0xffffff, 0xf0f0f0, 0xe0e0e0, 0xd0d0d0, 0xc0c0c0, 0xb0b0b0, 0xa0a0a0,
+    0x909090, 0x808080, 0x707070, 0x606060, 0x505050, 0x404040, 0x303030,
+    0x202020, 0x101010,
+  ]
 
-  tileTexture.wrapS = THREE.RepeatWrapping
-  tileTexture.wrapT = THREE.RepeatWrapping
-  tileTexture.repeat.set(20, 20)
+  function seededRandom(seed: number) {
+    const x = Math.sin(seed) * 10000
+    return x - Math.floor(x)
+  }
+
+  function getColor(i: number, j: number): Color {
+    const seed = i * 1000 + j
+    const randomIndex = Math.floor(seededRandom(seed) * monochromeColors.length)
+    return new Color(monochromeColors[randomIndex])
+  }
 </script>
 
-<T.Mesh receiveShadow position={[0, -0.5, 0]}>
-  <T.BoxGeometry args={[100, 1, 100]} />
-  <T.MeshStandardMaterial map={tileTexture} />
-</T.Mesh>
+{#each Array(20).fill(0) as _, i}
+  {#each Array(20).fill(0) as _, j}
+    <T.Mesh receiveShadow position={[i * 5 - 47.5, -0.5, j * 5 - 47.5]}>
+      <FakeGlowMaterial glowColor="blue" />
+      <T.BoxGeometry args={[5, 1, 5]} />
+      <T.MeshBasicMaterial
+        color={getColor(i, j)}
+        roughness={2.05}
+        metalness={0.9}
+        envMapIntensity={0.5}
+      />
+      <Edges color="white" />
+    </T.Mesh>
+  {/each}
+{/each}
