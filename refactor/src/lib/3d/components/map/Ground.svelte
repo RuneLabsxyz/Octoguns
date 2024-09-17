4fedf9b7dd3d7330f3ac7441c20e977ec1cfd67e
Veikkan <pierre.semanne@gmail.com>
2024-09-17 21:41:17 +0200

    efficient simple ground

--- refactor/src/lib/3d/components/map/Ground.svelte
+++ refactor/src/lib/3d/components/map/Ground.svelte
@@ -4,13 +4,8 @@
   import { Edges } from '@threlte/extras'
 </script>
 
-{#each Array(20).fill(0) as _, i}
-  {#each Array(20).fill(0) as _, j}
-    <T.Mesh receiveShadow position={[i * 5 - 47.5, -0.5, j * 5 - 47.5]}>
-      <FakeGlowMaterial glowColor="blue" />
-      <T.BoxGeometry args={[5, 0.5, 5]} />
-
-      <Edges color="white" />
-    </T.Mesh>
-  {/each}
-{/each}
+<T.Mesh receiveShadow position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+  <T.PlaneGeometry args={[100, 100]} />
+  <T.MeshPhysicalMaterial color="#ffffff" />
+  <Edges color="white" />
+</T.Mesh>
