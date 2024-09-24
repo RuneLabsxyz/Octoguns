3859df1e3c254e5794f62ccc495010a525f729d1
Veikkan <pierre.semanne@gmail.com>
2024-09-24 13:21:47 +0200

    feat: added arm with gun in fps mode

--- refactor/src/lib/3d/components/Cameras/SplitScreen.svelte
+++ refactor/src/lib/3d/components/Cameras/SplitScreen.svelte
@@ -5,6 +5,7 @@
   import { playerCharacterId } from '$stores/gameStores'
   import { playerCharacterCoords } from '$stores/coordsStores'
   import { birdView } from '$stores/cameraStores'
+  import Hand from '../models/hand.svelte'
 
   $: playerCoords = $playerCharacterCoords[$playerCharacterId]
 
@@ -23,5 +24,8 @@
       cameras[index] = ref
       ref.lookAt(0, 1, 0)
     }}
-  />
+  >
+    <!-- Position the hand 0.5 units in front of the camera -->
+    <Hand position={[0.1, -0.16, -0.6]} rotation={[0, Math.PI, 0]} />
+  </T.PerspectiveCamera>
 {/each}
