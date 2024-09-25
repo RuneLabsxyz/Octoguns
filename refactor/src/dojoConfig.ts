1405ecaba715c285f323d76ad64bfd57352b3597
Valentin Ricard <me@valentin.red>
2024-09-25 19:22:12 +0000

    feat: Added more environment variables + template

--- refactor/src/dojoConfig.ts
+++ refactor/src/dojoConfig.ts
@@ -1,12 +1,12 @@
 import manifest from '../../contracts/manifests/dev/deployment/manifest.json';
 import { createDojoConfig } from '@dojoengine/core';
-import { PUBLIC_TORII_URL } from '$env/static/public';
+
+import { PUBLIC_TORII_URL, PUBLIC_KATANA_URL } from '$env/static/public';
 
 export const dojoConfig = createDojoConfig({
-  toriiUrl: 'https://api.cartridge.gg/x/octoguns-demo/torii',
-  rpcUrl: 'https://api.cartridge.gg/x/octoguns-demo/katana',
+  toriiUrl: PUBLIC_TORII_URL,
+  rpcUrl: PUBLIC_KATANA_URL,
   manifest,
-  toriiUrl: PUBLIC_TORII_URL ,
 });
 
 export type Config = typeof dojoConfig;
