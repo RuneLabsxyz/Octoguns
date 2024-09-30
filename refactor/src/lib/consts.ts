b94081641904987346ad7edc042b1fb29e26e9ec
Mugen <45738409+0xMugen@users.noreply.github.com>
2024-09-30 22:23:01 +0200

    Update refactor/src/lib/consts.ts
    Co-authored-by: Valentin Ricard <me@valentin.red>

--- refactor/src/lib/consts.ts
+++ refactor/src/lib/consts.ts
@@ -2,7 +2,7 @@ export const MOVE_SPEED = 0.4
 export const BULLET_SPEED = 0.2
 export const SCALING_FACTOR = 1000
 
-export const RECORDING_FRAME_LIMIT = 300 // 300 frames = 100 moves
+export const RECORDING_FRAME_LIMIT = TURN_COUNT * FRAME_INTERVAL
 export const TURN_COUNT = 100
 export const FRAME_INTERVAL = 3
 // Scaling factor to map 100x100 to 100,000x100,000 grid
