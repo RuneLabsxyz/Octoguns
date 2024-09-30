ee35916da806bd8bfee0d6676ceae779932dd602
0xmugen <pierre.semanne@gmail.com>
2024-09-30 19:54:41 +0200

    fix: step bar adn set subsetp to 100

--- refactor/src/lib/consts.ts
+++ refactor/src/lib/consts.ts
@@ -2,7 +2,8 @@ export const MOVE_SPEED = 0.4
 export const BULLET_SPEED = 0.2
 export const SCALING_FACTOR = 1000
 
-export const RECORDING_FRAME_LIMIT = 150
+export const RECORDING_FRAME_LIMIT = 300 // 300 frames = 100 moves
+export const TURN_COUNT = 100
 export const FRAME_INTERVAL = 3
 // Scaling factor to map 100x100 to 100,000x100,000 grid
 export const SUBMOVE_SCALE = 400 // 0.4 * 1000
