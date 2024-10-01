b03844d1560dd9ae4daa2fd8d3c3d757bb14e0f4
0xmugen <pierre.semanne@gmail.com>
2024-10-01 19:40:33 +0200

    fix: rpc

--- refactor/src/dojoConfig.ts
+++ refactor/src/dojoConfig.ts
@@ -4,6 +4,8 @@ import { createDojoConfig } from '@dojoengine/core';
 
 export const dojoConfig = createDojoConfig({
   manifest,
+  toriiUrl: 'http://127.0.0.1:8080',
+  rpcUrl: 'http://127.0.0.1:5050',
 });
 
 export type Config = typeof dojoConfig;
