24a494d837f456296e54dfbdb60511a37c357e5b
Valentin Ricard <me@valentin.red>
2024-10-04 20:09:09 +0000

    fix: Fixed world address
    fix: Removed burner definitions

--- client/src/dojoConfig.ts
+++ client/src/dojoConfig.ts
@@ -1,7 +1,7 @@
 import manifest from '$manifests/deployment/manifest.json';
 import { createDojoConfig } from '@dojoengine/core';
 
-import {PUBLIC_TORII_URL, PUBLIC_RPC_URL, PUBLIC_BURNER_ADDRESS, PUBLIC_BURNER_KEY, PUBLIC_WORLD_ADDRESS} from '$env/static/public';
+import {PUBLIC_TORII_URL, PUBLIC_RPC_URL, PUBLIC_WORLD_ADDRESS} from '$env/static/public';
 
 export const WORLD_ADDRESS = PUBLIC_WORLD_ADDRESS;
 
@@ -9,8 +9,6 @@ export const dojoConfig = createDojoConfig({
   toriiUrl: PUBLIC_TORII_URL,
   rpcUrl: PUBLIC_RPC_URL,
   manifest,
-  masterAddress: PUBLIC_BURNER_ADDRESS,
-  masterPrivateKey: PUBLIC_BURNER_KEY
 });
 
 export type Config = typeof dojoConfig;
