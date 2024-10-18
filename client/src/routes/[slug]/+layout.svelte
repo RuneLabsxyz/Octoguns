1d2f68099d9c9c08d81ed6cc20876c70150cb319
Valentin Ricard <me@valentin.red>
2024-10-18 15:29:26 +0000

    fix: Redirect to main page when you are no longer logged in (or cancelled the login)

--- client/src/routes/[slug]/+layout.svelte
+++ client/src/routes/[slug]/+layout.svelte
@@ -2,16 +2,18 @@
   import { initializeStore } from '$stores/dojoStore'
   import { onMount } from 'svelte'
   import { env } from '$stores/network'
+  import { goto } from '$app/navigation'
+  import { account } from '$src/stores/account.js'
 
-  let isStoreInitialized = false;
+  let isStoreInitialized = false
 
   async function initStore() {
     try {
-      await initializeStore(true);
-      console.log('Store initialized');
-      isStoreInitialized = true; 
+      await initializeStore(true)
+      console.log('Store initialized')
+      isStoreInitialized = true
     } catch (error) {
-      console.error('Failed to initialize store:', error);
+      console.error('Failed to initialize store:', error)
     }
   }
 
@@ -19,9 +21,14 @@
   let network = data.network
 
   onMount(async () => {
-    env.set(network as "mainnet" | "slot");
-    await initStore();
-  });
+    env.set(network as 'mainnet' | 'slot')
+    await initStore()
+
+    if (!$account) {
+      console.warn('No account! Redirecting...')
+      goto('/')
+    }
+  })
 </script>
 
 {#if isStoreInitialized}
@@ -29,5 +36,3 @@
 {:else}
   <div>Loading...</div>
 {/if}
-
-
