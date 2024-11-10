bf935f84be24340cef772a366e7c88a1c0ce5629
Zack Ames <zames6584@gmail.com>
2024-11-10 08:59:49 +0700

    policies

--- client/src/lib/controller.ts
+++ client/src/lib/controller.ts
@@ -1,10 +1,14 @@
 import Controller from '@cartridge/controller';
 import { account, username } from '$stores/account';
+import { dojoStore } from '$src/stores/dojoStore';
+import { get } from 'svelte/store';
 
+let {planetelo }: any = get(dojoStore);
 export const mapmakerContract = '0x11e7a657668ca83c556f7545ab5bde00c1a1275c6c9ed17bea33104fcda2f3b'
 export const spawnContract = '0x57a72dd6f0bf3b0ced0fd50e54696643b8e3b38e226a150a1471c658355921e'
 export const startContract = '0x44ce069fe53c5c7941eec521bc9e876907d5da6ca273a98b78f1cbbcfea5b62'
 export const actionsContract = '0x11de1871e7fa8ac97e62c36724c32ee584982f4a5892b4f62fd545d7ab2d506'
+export const planeteloContract = planetelo.address
 
 export const controllerMainnet = new Controller({
   rpc: "https://api.cartridge.gg/x/starknet/mainnet"
@@ -15,7 +19,23 @@ export const controllerSlot = new Controller({
 })
 
 export const controllerSepolia = new Controller({
-  rpc: "https://api.cartridge.gg/x/starknet/sepolia"
+  rpc: "https://api.cartridge.gg/x/starknet/sepolia",
+  policies: [
+    {
+        target: planeteloContract,
+        method: "queue",
+    },
+    {
+        target: planeteloContract,
+        method: "matchmake",
+    },
+    {
+        target: planeteloContract,
+        method: "settle",
+    }
+
+    // ... other policies
+],
 })
 
 export async function connect(network: string) {
